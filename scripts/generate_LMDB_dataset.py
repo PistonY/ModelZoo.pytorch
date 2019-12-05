c073f1bc20195bdc98938bda645f3fa4f6fe6099
DevinYang <pistonyang@gmail.com>
2019-12-05 18:34:54 +0800

    update generate LMDB script.

--- scripts/generate_LMDB_dataset.py
+++ scripts/generate_LMDB_dataset.py
@@ -10,9 +10,6 @@ parser.add_argument('--data-dir', type=str, required=True,
                     help='ImageFolder path, this param will give to ImageFolder Dataset.')
 parser.add_argument('--save-dir', type=str, required=True,
                     help='Save dir.')
-parser.add_argument('--name', type=str, required=True,
-                    help='Save file name, need not to add `.lmdb`')
-parser.add_argument('--train', action='store_true', help='train or val dataset.')
 parser.add_argument('--download', action='store_true', help='download dataset.')
 parser.add_argument('-j', dest='num_workers', type=int, default=0)
 parser.add_argument('--write-frequency', type=int, default=5000)
@@ -21,10 +18,12 @@ parser.add_argument('--max-size', type=float, default=1.0,
                          '1T * `this param`')
 
 args = parser.parse_args()
-split = 'train' if args.train else 'val'
 check_dir(args.save_dir)
-data_set = ImageNet(args.data_dir, split, args.download, loader=raw_reader)
+train_data_set = ImageNet(args.data_dir, 'train', args.download, loader=raw_reader)
+val_data_set = ImageNet(args.data_dir, 'val', args.download, loader=raw_reader)
 
 if __name__ == '__main__':
-    generate_lmdb_dataset(data_set, args.save_dir, args.name, args.num_workers,
+    generate_lmdb_dataset(train_data_set, args.save_dir, 'train', args.num_workers,
+                          args.max_size, args.write_frequency)
+    generate_lmdb_dataset(val_data_set, args.save_dir, 'val', args.num_workers,
                           args.max_size, args.write_frequency)
